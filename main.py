a1811be62ddbbddc3f1c5c581df7c8928dab0424
aboredkeyboardwarrior <46739038+aboredkeyboardwarrior@users.noreply.github.com>
2019-03-27 16:52:41 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import praw
+import re
+import os
+
+if not os.path.isfile("comments_replied_to.txt"):
+    comments_replied_to = []
+
+else:
+    with open("comments_replied_to.txt", "r") as f:
+       comments_replied_to = f.read()
+       comments_replied_to = comments_replied_to.split("\n")
+       comments_replied_to = list(filter(None, comments_replied_to))
+       #removes blank entries
+       
+for submission in subreddit.hot(limit=10):
+  for comment in submission.comments:
+    if re.search("!cool", comment.body, re.IGNORECASE):
+      comment.reply("Thank you %s, very cool!" % (submission.author.name))
+      print("Bot replying to : ", comment.body)
+      comments_replied_to.append(comment.id)
+
+with open("comments_replied_to.txt", "w") as f:
+    for comment_id in comments_replied_to:
+      f.write(comment_id + "\n")
