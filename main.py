2ec6d923e14cf0a1c5338ee14135c0a20a8913c9
aboredkeyboardwarrior <46739038+aboredkeyboardwarrior@users.noreply.github.com>
2019-03-31 17:51:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import praw
+import pdb
 import re
 import os
+from praw.models import MoreComments
+
+
+# reddit = praw.Reddit('tyvc_bot', user_agent='bot2 user agent')
+reddit = praw.Reddit('tyvc_bot')
+subreddit = reddit.subreddit('pewdiepiesubmissions')
+
+# if not os.path.isfile("posts_replied_to.txt"):
+#     posts_replied_to = []
+
+# else:
+#     with open("posts_replied_to.txt", "r") as f:
+#        posts_replied_to = f.read()
+#        posts_replied_to = posts_replied_to.split("\n")
+#        posts_replied_to = list(filter(None, comments_replied_to))
+#        #removes blank entries
 
 if not os.path.isfile("comments_replied_to.txt"):
     comments_replied_to = []
@@ -14,6 +31,8 @@ else:
        
 for submission in subreddit.hot(limit=10):
   for comment in submission.comments:
+    if isinstance(comment, MoreComments):
+        continue
     if re.search("!cool", comment.body, re.IGNORECASE):
       comment.reply("Thank you /u/%s, very cool!" % (submission.author.name))
       print("Bot replying to : ", comment.body)
